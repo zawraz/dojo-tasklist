1d4f63e88b4f39d77d22071103186baca087ab0c
Konrad Sawras <zawraz@gmail.com>
2023-12-06 15:37:09 +0100

    Responsive layout setup

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,10 +5,16 @@ import { Grid, GridItem } from "@chakra-ui/react"
 export default function RootLayout() {
 	return (
 		<Grid templateColumns="repeat(6,1fr)" bg="gray.50">
-			<GridItem as="aside" colSpan="1" bg="purple.400" minH="100vh" p="30px">
+			<GridItem
+				as="aside"
+				colSpan={{ base: 6, lg: 2, xl: 1 }}
+				bg="purple.400"
+				minH={{ lg: "100vh" }}
+				p={{ base: "20px", lg: "30px" }}
+			>
 				<span>sidebar</span>
 			</GridItem>
-			<GridItem as="main" colSpan="5" p="40px">
+			<GridItem as="main" colSpan={{ base: 6, lg: 4, xl: 5 }} p="40px">
 				<Navbar />
 				<Outlet />
 			</GridItem>
