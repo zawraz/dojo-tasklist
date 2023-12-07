9c216577917edebd8901732682701ab46868970d
Konrad Sawras <zawraz@gmail.com>
2023-12-07 10:45:58 +0100

    Update Sidebar.jsx
    * fixed responsiveness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,26 @@
-import { List, ListItem } from "@chakra-ui/react"
+import { AtSignIcon, CalendarIcon, EditIcon } from "@chakra-ui/icons"
+import { List, ListIcon, ListItem } from "@chakra-ui/react"
 import { NavLink } from "react-router-dom"
 
 export default function Sidebar() {
 	return (
-		<List color="white" fontSize="1.2em" spacing="20px">
+		<List
+			color="white"
+			fontSize="1.2em"
+			gap="30px"
+			display="flex"
+			flexDirection={{ lg: "column" }}
+		>
 			<ListItem>
+				<ListIcon as={CalendarIcon} color="white" />
 				<NavLink to="/">Dashboard</NavLink>
 			</ListItem>
 			<ListItem>
+				<ListIcon as={EditIcon} color="white" />
 				<NavLink to="/create">New task</NavLink>
 			</ListItem>
 			<ListItem>
+				<ListIcon as={AtSignIcon} color="white" />
 				<NavLink to="/profile">Profile</NavLink>
 			</ListItem>
 		</List>
