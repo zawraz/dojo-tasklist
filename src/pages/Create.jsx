4ab311591e010c74fef3f8abafb47d4cd59d46ab
Konrad Sawras <zawraz@gmail.com>
2023-12-11 10:51:54 +0100

    'New task' form
    * +form for adding new tasks to the dashboard
    * modified App routing

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,53 @@
+import {
+	Box,
+	Button,
+	Checkbox,
+	FormControl,
+	FormHelperText,
+	FormLabel,
+	Input,
+	Textarea,
+} from "@chakra-ui/react"
+import { Form, redirect } from "react-router-dom"
+
 export default function Create() {
-  return (
-    <div>Create</div>
-  )
+	return (
+		<Box maxW="480px" action="/create">
+			<Form method="post">
+				<FormControl isRequired mb="40px">
+					<FormLabel>Task name:</FormLabel>
+					<Input type="text" name="title" />
+					<FormHelperText>Enter a task name</FormHelperText>
+				</FormControl>
+				<FormControl mb="10px">
+					<FormLabel>Task description:</FormLabel>
+					<Textarea
+						placeholder="Enter a detailed description for a task..."
+						name="description"
+					/>
+				</FormControl>
+				<FormControl display="flex" alignItems="center" mb="40px">
+					<Checkbox name="isPriority" size="lg" colorScheme="purple" />
+					<FormLabel mb="0px" ml="10px">
+						Make this a priority task.
+					</FormLabel>
+				</FormControl>
+				<Button type="submit">Submit</Button>
+			</Form>
+		</Box>
+	)
+}
+
+export const createAction = async ({ request }) => {
+	const data = await request.formData()
+
+	const task = {
+		title: data.get("title"),
+		description: data.get("description"),
+		isPriority: data.get("isPriority") === "",
+	}
+
+	console.log(task)
+
+	return redirect("/")
 }
