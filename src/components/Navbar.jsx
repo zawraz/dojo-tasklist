13bce3efc70714ac34c70e569917b5d3270767ef
Konrad Sawras <zawraz@gmail.com>
2023-12-11 11:40:12 +0100

    Update Navbar.jsx
    * +toast message upon logging out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { UnlockIcon } from "@chakra-ui/icons"
 import {
 	Box,
 	Button,
@@ -6,9 +7,24 @@ import {
 	Heading,
 	Spacer,
 	Text,
+	useToast,
 } from "@chakra-ui/react"
 
 export default function Navbar() {
+	const toast = useToast()
+
+	const showToast = () => {
+		toast({
+			title: "Logged out",
+			description: "Successfully logged out",
+			duration: 2500,
+			isClosable: true,
+			status: "success",
+			position: "top",
+			icon: <UnlockIcon />,
+		})
+	}
+
 	return (
 		<Flex as="nav" p="10px" mb="40px" align="center">
 			<Heading as="h1">Dojo Tasklist</Heading>
@@ -18,7 +34,9 @@ export default function Navbar() {
 					M
 				</Box>
 				<Text>test1@yopmail.com</Text>
-				<Button colorScheme="purple">Log out</Button>
+				<Button colorScheme="purple" onClick={showToast}>
+					Log out
+				</Button>
 			</HStack>
 		</Flex>
 	)
